720d7b14da3877a84d436382b29fb6748d120f58
Łukasz Kowalski <96295532+lukasz85BTM@users.noreply.github.com>
2022-01-21 23:13:42 +0100

    Add files via upload

---
+++ Targecik/Alert1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Targecik
+{
+    public partial class Form_Alert : Form
+    {
+        public Form_Alert()
+        {
+            InitializeComponent();
+        }
+
+        public enum enmAction
+        {
+            wait,
+            start,
+            close
+        }
+
+        private enmAction action;
+        private int x;
+        private int y;
+
+        public void FormAlert(string msg)
+        {
+            this.Opacity = 0.0;
+            this.StartPosition = FormStartPosition.Manual;
+            string fname;
+
+            for (int i = 0; i < 10; i++)
+            {
+                fname = "alert" + i.ToString();
+                Form_Alert frm = (Form_Alert)Application.OpenForms[fname];
+
+                if (frm == null)
+                {
+                    this.Name = fname;
+                    this.x = Screen.PrimaryScreen.WorkingArea.Width - this.Width + 15;
+                    this.y = Screen.PrimaryScreen.WorkingArea.Height - this.Height * i;
+                    this.Location = new Point(this.x, this.y);
+                    break;
+                }
+            }
+        }
+
+        private void Alert1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
